c7ad6148c19ce028218b9839b7b7c2ac22dc0783
david <david@livingroompc>
2016-09-25 14:44:52 +0100

    Add hola mundo message

--- MyFirstJavaApp.java
+++ MyFirstJavaApp.java
@@ -19,6 +19,7 @@ public class MyFirstJavaApp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
+        System.out.println("Hola Mundo!");
         
         Drinks d = new Bottle(10);
         System.out.println("Size = " + d.getSize()+" ml");
